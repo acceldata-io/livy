32dafcb765a7b789755e830763a9c6646571b7a9
Prabhjyot Singh <prabhjyot@acceldata.io>
2024-09-24 21:47:51 -0400

    ODP-2050: upgrade bootstrap to 4.6.2

--- server/src/main/scala/org/apache/livy/server/ui/UIServlet.scala
+++ server/src/main/scala/org/apache/livy/server/ui/UIServlet.scala
@@ -33,13 +33,15 @@ class UIServlet(val basePath: String, livyConf: LivyConf) extends ScalatraServle
   private case class SimplePage(name: String) extends Page
 
   private case class AllSessionsPage(name: String = "Sessions") extends Page {
-    override def getNavCrumbs: Seq[Node] = <li class="active"><a href="#">Sessions</a></li>
+    override def getNavCrumbs: Seq[Node] = {
+      <li class="active"><a class="nav-link" href="#">Sessions</a></li>
+    }
   }
   private case class SessionPage(id: Int) extends Page {
     val name: String = "Session " + id
     override def getNavCrumbs: Seq[Node] = {
-        <li><a href={basePath + "/ui"}>Sessions</a></li> ++
-          <li class="active"><a href="#">{name}</a></li>
+        <li><a class="nav-link" href={basePath + "/ui"}>Sessions</a></li> ++
+          <li class="active"><a class="nav-link" href="#">{name}</a></li>
     }
   }
   private case class LogPage(sessionType: String, id: Int) extends Page {
@@ -51,9 +53,9 @@ class UIServlet(val basePath: String, livyConf: LivyConf) extends ScalatraServle
       } else {
         "#"
       }
-      <li><a href={basePath + "/ui"}>Sessions</a></li> ++
-        <li><a href={sessionLink}>{sessionName}</a></li> ++
-        <li class="active"><a href="#">Log</a></li>
+      <li><a class="nav-link" href={basePath + "/ui"}>Sessions</a></li> ++
+        <li><a class="nav-link" href={sessionLink}>{sessionName}</a></li> ++
+        <li class="active"><a class="nav-link" href="#">Log</a></li>
     }
   }
 
@@ -78,7 +80,7 @@ class UIServlet(val basePath: String, livyConf: LivyConf) extends ScalatraServle
     </head>
 
   private def wrapNavCrumbs(crumbs: Seq[Node]): Seq[Node] =
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-expand navbar-light navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
           <a class="navbar-brand" href={basePath + "/ui"}>
